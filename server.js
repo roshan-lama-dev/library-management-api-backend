1a7fc6710a17c49e6e4a0904939c94535f4c6d93
Havochalo <officialmail85@gmail.com>
2023-02-07 18:44:12 +1100

    Backend initialised

--- server.js
+++ server.js
@@ -1,45 +1,29 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { dbConnection } from "./src/dbConfig/dbConfig.js";
-dotenv.config();
+
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// uncaught url handling
 
-dbConnection();
-
-// creating the endp point for the userRouter
-// middleware
-app.use(express.json());
-app.use(cors());
-// importing the userRouter
-import useRouter from "./src/router/UserRouter.js";
-import { ERROR } from "./src/constant.js";
-app.use("/api/v1/user", useRouter);
-
-// bad url endpoint handling
 app.use("*", (req, res, next) => {
-  const errorObj = {
-    statusCode: 404,
-    messge: "Page not found",
+  const error = {
+    errorCode: 404,
+    message: "Page not found",
   };
-  next(errorObj);
+  next(error);
 });
 
 // global error handling
 app.use((error, req, res, next) => {
   try {
-    const statusCode = error.statusCode || 500;
-    res.status(statusCode).json({
-      status: ERROR,
+    const errorCode = error.errorCode || 500;
+    res.status(errorCode).json({
+      status: "error",
       message: error.message,
     });
   } catch (error) {
-    console.log(error.message);
-
     res.json({
-      status: ERROR,
+      status: "error",
       message: error.message,
     });
   }
@@ -47,6 +31,6 @@ app.use((error, req, res, next) => {
 
 app.listen(PORT, (error) => {
   error
-    ? console.log(error)
-    : console.log(`Your server is running at http://localhost:${PORT}`);
+    ? console.log(error.message)
+    : console.log(`The server is running at http://localhost:${PORT}`);
 });
